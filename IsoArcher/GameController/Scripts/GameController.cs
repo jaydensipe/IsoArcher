b9980cd268b5ccdb5c3ca0346b0116b6d59b6721
Jayden Sipe <jayden.sipe@gmail.com>
2020-06-14 21:00:45 -0400

    Added a wave system for spawning enemies

--- IsoArcher/GameController/Scripts/GameController.cs
+++ IsoArcher/GameController/Scripts/GameController.cs
@@ -10,13 +10,42 @@ public class GameController : Node
     private PackedScene testEnemy = new PackedScene();
     private readonly Random rng = new Random();
     private readonly Array<Position3D> positionArray = new Array<Position3D>();
+
+    // Wave spawning enemy logic
+    private int currentWave = 0;
+    private bool waveEnded = false;
     
     public override void _Ready()
     {
-        positionArray.Add(GetNode<Position3D>("World1/EnemySpawnPositions/Position3D"));
-        positionArray.Add(GetNode<Position3D>("World1/EnemySpawnPositions/Position3D2"));
-        positionArray.Add(GetNode<Position3D>("World1/EnemySpawnPositions/Position3D3"));
-        positionArray.Add(GetNode<Position3D>("World1/EnemySpawnPositions/Position3D4"));
+        positionArray.Add(GetNode<Position3D>("CurrentWorld/World1/EnemySpawnPositions/Position3D"));
+        positionArray.Add(GetNode<Position3D>("CurrentWorld/World1/EnemySpawnPositions/Position3D2"));
+        positionArray.Add(GetNode<Position3D>("CurrentWorld/World1/EnemySpawnPositions/Position3D3"));
+        positionArray.Add(GetNode<Position3D>("CurrentWorld/World1/EnemySpawnPositions/Position3D4"));
+        
+    }
+
+    // Debug to spawn waves
+    public override void _Process(float delta)
+    {
+        if (Input.IsActionJustPressed("TestWave"))
+        {
+            WaveSystem();
+        }
+    }
+
+    // Controls the waves and how the enemies spawn, and spawns an enemy every x seconds
+    private async void WaveSystem()
+    {
+        waveEnded = false;
+        currentWave += 1;
+
+        for (int i = 0; i < Math.Ceiling(currentWave*4.5); i++)
+        {
+            SpawnEnemy(positionArray);
+            await ToSignal(GetNode<Timer>("Timers/EnemySpawnTimer"), "timeout");
+        }
+
+        waveEnded = true;
     }
 
     private void SpawnEnemy(Array<Position3D> positionArraysForSpawning)
@@ -33,10 +62,6 @@ public class GameController : Node
         
     }
 
-    // Spawns an enemy every x seconds
-    void _on_EnemySpawnTimer_timeout()
-    {
-        SpawnEnemy(positionArray);
-    }
-    
+ 
+
 }
